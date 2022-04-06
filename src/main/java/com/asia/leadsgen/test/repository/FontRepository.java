a0439eb1a8c424d451afd472522a218a5da16f57
Trường <83860817+sdfghesc333333@users.noreply.github.com>
2022-04-06 16:03:01 +0700

    Create FontRepository.java

---
+++ src/main/java/com/asia/leadsgen/test/repository/FontRepository.java
@@ -0,0 +1,16 @@
+package com.asia.leadsgen.test.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.asia.leadsgen.test.model.CampaignEntity;
+import com.asia.leadsgen.test.model.FontEntity;
+
+@Repository
+public interface FontRepository extends JpaRepository<FontEntity, Long> {
+	Page<CampaignEntity> findAllByUserId(Pageable pageable, Long userId);
+	
+	CampaignEntity findByIdAndUserIdAndStatus(Long id, Long userId, int status);
+}
