c307c4ca0076eba1f405081f38d0a99e4bed8a22
Trường <83860817+sdfghesc333333@users.noreply.github.com>
2022-04-15 14:47:29 +0700

    stats api home controller

--- src/main/java/com/asia/leadsgen/test/controller/HomeController.java
+++ src/main/java/com/asia/leadsgen/test/controller/HomeController.java
@@ -1,5 +1,59 @@
 package com.asia.leadsgen.test.controller;
 
+import java.text.ParseException;
+import java.util.Map;
+
+import javax.security.auth.login.LoginException;
+
+import org.apache.commons.collections4.map.HashedMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.asia.leadsgen.test.model.UserInfo;
+import com.asia.leadsgen.test.repository.CampaignRepository;
+import com.asia.leadsgen.test.service.UserService;
+import com.asia.leadsgen.test.util.AppParams;
+
+import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping(path = "/stats")
 public class HomeController {
 
+	@Autowired
+	CampaignRepository campaignRepository;
+
+	@Autowired
+	UserService userService;
+
+//	Route::get('/stats', [HomeController::class, 'stats']);
+	@GetMapping()
+	@Operation(summary = "Count")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "OK"),
+			@ApiResponse(responseCode = "201", description = "Not implement", content = @Content),
+			@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+			@ApiResponse(responseCode = "403", description = "Not implement", content = @Content),
+			@ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
+			@ApiResponse(responseCode = "500", description = "Internal Sever Error", content = @Content) })
+	public ResponseEntity<Map<String, Integer>> stats(
+			@RequestHeader(name = "x-authorization", required = true) @ApiParam(value = "Access Token", example = AppParams.TOKEN) String accessToken,
+			@RequestAttribute(name = "user_info", required = true) UserInfo userInfo)
+			throws LoginException, ParseException {
+
+		Map<String, Integer> result = new HashedMap<String, Integer>();
+		result.put("total_campaigns ", campaignRepository.countTotal30dayago(userService.getUser(userInfo).getId()));
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
 }
--- src/main/java/com/asia/leadsgen/test/repository/CampaignRepository.java
+++ src/main/java/com/asia/leadsgen/test/repository/CampaignRepository.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.asia.leadsgen.test.model.entity.CampaignEntity;
@@ -18,4 +19,7 @@ public interface CampaignRepository extends JpaRepository<CampaignEntity, Long> 
 
 	Page<CampaignEntity> findAllByUserIdAndDeletedAtAndNameLikeAndCreatedAtBetween(Pageable pageable, Long userId, Date deletedAt,
 			String name, Date startDate, Date endDate);
+	
+	@Query(value = "select count(*) from campaigns where created_at > (CURRENT_DATE - INTERVAL 30 DAY) and user_id = ? and deleted_at is null",nativeQuery = true)
+    int countTotal30dayago(Long userId);
 }
